7159b28e0ee9ac5b7220cff44f0a239c94d319e4
Sun Haoran <ludanxer@gmail.com>
2018-12-19 12:14:36 +0800

    update for submit

--- back-end/create_table.py
+++ back-end/create_table.py
@@ -1,26 +1,6 @@
-from pymongo import MongoClient
-
-client = MongoClient()
-
-db = client['movie']
-
-collection = db['cart']
-
-document = {
-    'id': '123456',
-    'title': 'cart',
-    'actor': 'ludanxer',
-    'director': 'lucass',
-    'time': '2018',
-    'price': 29.99,
-    'genre': 'Action',
-    'review': 10
-}
-collection.insert_one(document)
+import pyhive
 
 # cursor = hive.connect(host='localhost', port='10000').cursor()
-# conn = sqlite3.connect('movieshop.db')
-# cursor = conn.cursor()
 
 # cursor.execute('create table cart(id varchar(50), title varchar(50), actor varchar(50), director varchar(50), \
 #                 time varchar(50), price float, genre varchar(50), review int)')
